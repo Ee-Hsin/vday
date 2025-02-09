ae281b76c9fcb609d7cb682b6cf603bb9f220be9
Jordan (Ee Hsin) <eehsinkok777@gmail.com>
2025-02-08 20:30:59 -0500

    fixed fallback stamp to ensure can't click into it

--- components/FramedImage.tsx
+++ components/FramedImage.tsx
@@ -4,7 +4,6 @@ import ImageModal from "./ImageModal"
 import { Fredoka, Poppins } from "next/font/google"
 import stamp1 from "@/assets/stamp 1.png"
 
-
 interface FramedImageProps {
   src: string
   alt: string
@@ -33,8 +32,12 @@ export default function FramedImage({
   return (
     <>
       <div
-        className={`relative p-4 bg-[#f7eaee] hover:bg-[#efcdd0] transition-colors duration-200 shadow-[0_0px_30px_0px_rgba(244,114,182,0.2),0_0px_20px_-5px_rgba(0,0,0,0.4)] rounded-lg cursor-pointer w-[200px] ${className}`}
-        onClick={() => setIsModalOpen(true)}
+        className={`relative p-4 bg-[#f7eaee] hover:bg-[#efcdd0] transition-colors duration-200 shadow-[0_0px_30px_0px_rgba(244,114,182,0.2),0_0px_20px_-5px_rgba(0,0,0,0.4)] rounded-lg ${src != "fallbackStamp" && "cursor-pointer"} w-[200px] ${className}`}
+        onClick={() => {
+          if (src != "fallbackStamp") {
+            setIsModalOpen(true)
+          }
+        }}
       >
         <div className="relative flex flex-col">
           {/* Fixed square image container */}
